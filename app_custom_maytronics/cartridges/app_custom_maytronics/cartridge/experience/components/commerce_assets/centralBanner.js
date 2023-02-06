55e78c4ded204ab5c473176d6e26bdaa7286dfb5
Or Bashan <orbashan94@gmail.com>
2023-02-06 11:11:10 +0200

    Update centralBanner.js

--- app_custom_maytronics/cartridges/app_custom_maytronics/cartridge/experience/components/commerce_assets/centralBanner.js
+++ app_custom_maytronics/cartridges/app_custom_maytronics/cartridge/experience/components/commerce_assets/centralBanner.js
@@ -6,13 +6,7 @@ var HashMap = require('dw/util/HashMap');
 var Categories = require('*/cartridge/models/categories');
 var ArrayList = require('dw/util/ArrayList');
 
-/**
- * Render logic for the storefront.category component.
- * @param {dw.experience.ComponentScriptContext} context The Component script context object.
- * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commerce Cloud Platform.
- *
- * @returns {string} The markup to be displayed
- */
+
 module.exports.render = function (context, modelIn) {
     var model = modelIn || new HashMap();
     var content = context.content;
@@ -22,11 +16,11 @@ module.exports.render = function (context, modelIn) {
         focalPointX: (content.image.focalPoint.x * 100) + '%',
         focalPointY: (content.image.focalPoint.y * 100) + '%'
     };
-
+    
     if(content.alt) {
         model.alt = content.alt;
     }
-    
+
     if(content.textHeadline) {
         model.textHeadline = content.textHeadline;
     }
@@ -41,23 +35,14 @@ module.exports.render = function (context, modelIn) {
 
     model.changeTextColor = content.changeTextColor;
 
-
     var categoriesToBeDisplayed = new ArrayList();
     var customCategoryNames = new HashMap();
-
 
     var cat = content['category1'];
     categoriesToBeDisplayed.push(cat);       
     var categories = new Categories(categoriesToBeDisplayed);
     model.categories = categories.categories;
     model.customCategoryNames = customCategoryNames;
-
-    
-
-    // instruct 24 hours relative pagecache
-    var expires = new Date();
-    expires.setDate(expires.getDate() + 1); // this handles overflow automatically
-    response.setExpires(expires);
 
     return new Template('experience/components/commerce_assets/centralBanner').render(model).text;
 };
